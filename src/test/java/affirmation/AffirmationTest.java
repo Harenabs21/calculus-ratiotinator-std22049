91f5dc787c4f52a01dbaacf632422a454ec35997
Harenabs21 <hei.harena.6@gmail.com>
2024-06-07 16:30:20 +0300

    feat(test): tester qu'on peut faire une affirmation

---
+++ src/test/java/affirmation/AffirmationTest.java
@@ -0,0 +1,14 @@
+package affirmation;
+
+import calculus.std22049.affirmations.Affirmation;
+import org.junit.jupiter.api.Test;
+
+
+public class AffirmationTest {
+
+    @Test
+    public void faire_une_affirmation() {
+        Affirmation affirmation = new Affirmation("Je mange une glace");
+    }
+
+}
