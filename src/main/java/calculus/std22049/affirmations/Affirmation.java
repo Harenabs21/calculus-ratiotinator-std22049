4fabaa43c8c82f4c811cfc002ae07ca9308cb2cd
Harenabs21 <hei.harena.6@gmail.com>
2024-06-12 09:11:09 +0300

    chore: private phrase -> protected phrase

--- src/main/java/calculus/std22049/affirmations/Affirmation.java
+++ src/main/java/calculus/std22049/affirmations/Affirmation.java
@@ -2,10 +2,16 @@ package calculus.std22049.affirmations;
 
 import lombok.Getter;
 
+import static calculus.std22049.affirmations.ValeurDeVerite.jenesaispas;
+
 @Getter
 public sealed class Affirmation permits Verite, Mensonge {
-    private String phrase;
+    protected String phrase;
     public Affirmation(String phrase){
         this.phrase = phrase;
+    }
+
+    public ValeurDeVerite valeurDeVerite() {
+        return jenesaispas;
     }
 }
